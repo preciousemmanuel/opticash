b3ba5a5c7a33e82f34e8a839e71424756d11a752
Precious Emmanuel <precious.emmanuel@cicod.com>
2023-12-11 18:14:42 +0100

    first commit

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import "dotenv/config";
 import "module-alias/register";
 import validateEnv from '@/utils/validateEnv';
 import App from "./app";
-import UserController from "./resources/user/user.controller";
-import PinController from "./resources/pin/pin.controller";
-import WalletController from "./resources/wallet/wallet.controller";
-import TransactionController from "./resources/transaction/transaction.controller";
+import UserController from "@/resources/user/user.controller";
+import PinController from "@/resources/pin/pin.controller";
+import WalletController from "@/resources/wallet/wallet.controller";
+import TransactionController from "@/resources/transaction/transaction.controller";
 
 validateEnv();
 
